2df26f6e776e49b66e8ccab7703267af38fd945e
Daniel Jay Haskin <djhaskin987@gmail.com>
2015-12-26 23:23:32 -0700

    Rethinking resolve

--- src/repository.hpp
+++ src/repository.hpp
@@ -5,13 +5,13 @@
    you may not use this file except in compliance with the License.
    You may obtain a copy of the License at
 
-       http://www.apache.org/licenses/LICENSE-2.0
+http://www.apache.org/licenses/LICENSE-2.0
 
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 */
 #ifndef UDR_REPOSITORY_HPP
 #define UDR_REPOSITORY_HPP 1
@@ -19,86 +19,60 @@
 #include <algorithm>
 #include <string>
 
+namespace udr {
+    typedef std::string URL;
+    typedef std::string name_type;
+    typedef std::string version_type;
+    typedef std::function<bool(version_type)> constraint_type;
+    typedef std::function<int(version_type,version_type)> compare_type;
+    typedef std::function<std::map<versioN_type, URL>(name_type) > query_type;
+    typedef std::function<std::map<name_type, std::set<constraint_type> >(
+            name_type, version_type)> query_dependencies_type;
 
-struct constraint
-{
-    std::string version;
-    std::string constraint;
-};
+    std::list<URL> resolve(
+            const std::map<name, std::set<constraint_type> > & order,
+            const compare_type & compare_versions,
+            query_type query,
+            query_dependencies_type dependencies) {
 
-struct file
-{
-    std::string url;
-    std::string checksum;
-};
+        std::list<URL> result;
+        for (auto package : order) {
+            auto name = package.first;
+            auto constraints = package.second;
 
-// this is a concrete class.
-class package_behavior_profile
-{
-private:
-    std::function<bool(std::string)> validate_name;
-    std::function<bool(std::string)> validate_version;
-    std::function<int(std::string,std::string)> compare_versions;
-    std::function<bool(constraint)> validate_constraint;
-    std::function<bool(constraint,std::string)> check_constraint;
-public:
-    const std::function<bool(std::string)> validate_name() {
-        return validate_name;
-    }
-    const std::function<bool(std::string)> validate_version() {
-        return validate_version;
-    }
-    const std::function<int(std::string,std::string)> compare_versions() {
-        return compare_versions;
-    }
-    const std::function<bool(std::string)> satisfies(constraint c) {
-        return [this->check_constraint,c](std::string ver) {
-            return this->check_constraint(c, ver);
-        };
-    }
-};
+            std::map<version_type, URL> candidates = query(name);
+            for (auto constraint : constraints) {
+                std::remove_if(candidates.begin(), candidates.end(),
+                        [constraint&](auto x){return constraint(x.first);});
+            }
 
-class respository {
-private:
-    package_behavior_profile package_behavior;
-    std::function<std::map<version,file>(const std::string &)> query;
-    std::function<std::map<name, std::list<constraint>(const std::string &, const std::string &) > query_dependencies;
-public:
-    std::list<file> resolve(const std::map<name, std::list<constraint> > order) {
+            if (candidates.empty() ) {
+                return result;
+            }
 
-    std::list<file> returned;
-    for (auto package : order) {
-        std::string name = package.first;
-        std::list<constraint> constraints = package.second;
-        std::map<version, file> candidates = this->query(name);
+            std::stable_sort(candidates,
+                    [](a, b) {
+                    return compare_versions(a,b) * -1;
+                    });
 
-        for (auto constraint : constraints) {
-            std::remove_if(candidates.begin(), candidates.end(),
-                    this->package_behavior.satisfies(constraint))
-        }
-
-        if (candidates.empty() ) {
-            return returned;
-        }
-
-        candidates = sort(candidates,
-            []{ return -1 * this->package_behavior.compare_versions(); } );
-
-        for (auto candidate : candidates) {
-            auto candidate_version = candidate.first;
-            auto candidate_file = candidate.second;
-            auto candidate_deps =
-                this->query_dependencies(name, candidate_version);
-            auto candidate_dep_files = this->resolve(candidate_deps);
-            if (!candidate_dep_files.empty()) {
-                // concatenate lists, use move semantics if possible
-                returned = returned + candidate_dep_files;
-
-                returned.push_back(candidate_file);
-                break;
+            for (auto candidate : candidates) {
+                auto candidate_version = candidate.first;
+                auto candidate_url = candidate.second;
+                auto candidate_deps =
+                    dependencies(name, candidate_version);
+                auto candidate_dep_urls = resolve(candidate_deps,
+                        compare,
+                        query,
+                        dependencies);
+                if (!candidate_dep_urls.empty()) {
+                    // concatenate lists, use move semantics if possible
+                    result.splice(result.cend(), candidate_dep_files);
+                    break;
+                }
+                result.push_back(candidate_url);
             }
         }
+        return result;
     }
-    }
-};
+}
 #endif // UDR_REPOSITORY_HPP
