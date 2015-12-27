cc1bc09514cc739bade21c6adcff4880c7d6fbb6
Daniel Jay Haskin <djhaskin987@gmail.com>
2015-12-27 00:28:37 -0700

    Use classes in the resolve function

--- src/repository.hpp
+++ src/repository.hpp
@@ -24,55 +24,71 @@ namespace udr {
     typedef std::string name_type;
     typedef std::string version_type;
     typedef std::function<bool(version_type)> constraint_type;
-    typedef std::function<int(version_type,version_type)> compare_type;
-    typedef std::function<std::map<versioN_type, URL>(name_type) > query_type;
-    typedef std::function<std::map<name_type, std::set<constraint_type> >(
-            name_type, version_type)> query_dependencies_type;
 
-    std::list<URL> resolve(
-            const std::map<name, std::set<constraint_type> > & order,
-            const compare_type & compare_versions,
-            query_type query,
-            query_dependencies_type dependencies) {
 
-        std::list<URL> result;
-        for (auto package : order) {
-            auto name = package.first;
-            auto constraints = package.second;
+    class package_system {
+        public:
+            virtual std::unordered_map<name_type, std::unordered_set<constraint_type> >
+                query_dependencies(
+                        const name_type & name,
+                        const version_type & version) = 0;
+            virtual std::unordered_map<version_type, URL> query_candidates(
+                    const name_type & name) = 0;
+            virtual int compare_versions(const version_type & a,
+                    const version_type & b) = 0;
+            virtual std::unordered_map<name_type, std::unordered_set<constraint_type> >
+                constraints_from_string(
+                        const std::string & constraints) = 0;
+    };
 
-            std::map<version_type, URL> candidates = query(name);
-            for (auto constraint : constraints) {
-                std::remove_if(candidates.begin(), candidates.end(),
-                        [constraint&](auto x){return constraint(x.first);});
-            }
+    class repository {
+        private:
+            const package_system *system;
+            std::forward_list<URL> resolve(const std::unordered_map<name_type, std::unordered_set<constraint_type> &
+                    order) const {
+                std::forward_list<URL> result;
+                for (auto package : order) {
+                    auto name = package.first;
+                    auto constraints = package.second;
 
-            if (candidates.empty() ) {
+                    std::unordered_map<version_type, URL> candidates =
+                        system->query_candidates(name);
+                    for (auto constraint : constraints) {
+                        std::remove_if(candidates.begin(), candidates.end(),
+                                [constraint&](auto x){return constraint(x.first);});
+                    }
+
+                    if (candidates.empty() ) {
+                        return result;
+                    }
+
+                    // sort in descending order, latest versions first
+                    std::stable_sort(candidates,
+                            [](a, b) {
+                            return system->compare_versions(a,b) * -1;
+                            });
+
+                    for (auto candidate : candidates) {
+                        auto candidate_version = candidate.first;
+                        auto candidate_url = candidate.second;
+                        auto candidate_deps =
+                            system->query_dependencies(name, candidate_version);
+                        auto candidate_dep_urls = this->resolve(candidate_deps);
+                        if (!candidate_dep_urls.empty()) {
+                            // concatenate lists, use move semantics if possible
+                            result.splice(result.cend(), candidate_dep_files);
+                            break;
+                        }
+                        result.push_back(candidate_url);
+                    }
+                }
                 return result;
             }
-
-            std::stable_sort(candidates,
-                    [](a, b) {
-                    return compare_versions(a,b) * -1;
-                    });
-
-            for (auto candidate : candidates) {
-                auto candidate_version = candidate.first;
-                auto candidate_url = candidate.second;
-                auto candidate_deps =
-                    dependencies(name, candidate_version);
-                auto candidate_dep_urls = resolve(candidate_deps,
-                        compare,
-                        query,
-                        dependencies);
-                if (!candidate_dep_urls.empty()) {
-                    // concatenate lists, use move semantics if possible
-                    result.splice(result.cend(), candidate_dep_files);
-                    break;
-                }
-                result.push_back(candidate_url);
+        public:
+            std::forward_list<URL> resolve(const std::string & constraints) const {
+                return resolve(system->constraints_from_string(constraints));
             }
-        }
-        return result;
-    }
+    };
 }
+
 #endif // UDR_REPOSITORY_HPP
