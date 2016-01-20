d1110fd46108c2931bbe6a7ba1d7306eea017efa
Daniel Jay Haskin <djhaskin987@gmail.com>
2016-01-19 17:26:12 -0700

    Some more output in tests, pls

--- test/test_gripe.cpp
+++ test/test_gripe.cpp
@@ -7,9 +7,18 @@
 #define UDR_UNIT_TEST
 #include "gripe.hpp"
 
+BOOST_AUTO_TEST_SUITE( gripe )
+
 BOOST_AUTO_TEST_CASE( gripe_age )
 {
     udr::Gripe g;
     BOOST_CHECK ( g.is(1) );
 }
+
+BOOST_AUTO_TEST_CASE( gripe_fail )
+{
+    BOOST_FAIL(true);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
 
