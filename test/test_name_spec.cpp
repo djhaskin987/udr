949e39e6ab8644a2160f826c15642294e68f36d4
Daniel Haskin <djhaskin987@gmail.com>
2016-06-02 13:04:53 -0600

    Ensure assertions are always made in name_spec tests

--- test/test_name_spec.cpp
+++ test/test_name_spec.cpp
@@ -26,32 +26,25 @@ BOOST_FIXTURE_TEST_CASE(successful_retrieval, hello_repo)
 {
     auto tested = make_name_spec<std::string, int>("hello");
     auto results = tested->resolve(&mr);
-    if (emessages_type* errors = boost::get<emessages_type>(&results))
-    {
-        BOOST_ERROR(std::string("Errors."));
-    }
-    else if (std::vector<package<std::string, int>>* packages =
-            boost::get<std::vector<package<std::string, int>>>(&results))
-    {
-        BOOST_CHECK(packages->size() == 1);
-        package<std::string, int> expected = { "hello", 45, "hello_loc" };
-        BOOST_CHECK(packages->begin()->name == expected.name);
-        BOOST_CHECK(packages->begin()->version == expected.version);
-        BOOST_CHECK(packages->begin()->location == expected.location);
-    }
+    emessages_type* errors = boost::get<emessages_type>(&results);
+    BOOST_CHECK(errors == nullptr);
+    std::vector<package<std::string, int>>* packages =
+            boost::get<std::vector<package<std::string, int>>>(&results);
+    BOOST_REQUIRE(packages != nullptr);
+    BOOST_CHECK(packages->size() == 1);
+    package<std::string, int> expected = { "hello", 45, "hello_loc" };
+    BOOST_CHECK(packages->begin()->name == expected.name);
+    BOOST_CHECK(packages->begin()->version == expected.version);
+    BOOST_CHECK(packages->begin()->location == expected.location);
 }
 
 BOOST_FIXTURE_TEST_CASE(nothing_found, hello_repo)
 {
     auto tested = make_name_spec<std::string, int>("goodbye");
     auto results = tested->resolve(&mr);
-    if (std::vector<package<std::string,int>>* packages =
-            boost::get<std::vector<package<std::string, int>>>(&results))
-    {
-        BOOST_ERROR(std::string() +
-                "An error should have been reported when querying for " +
-                "a non existent package.");
-    }
+    std::vector<package<std::string,int>>* packages =
+            boost::get<std::vector<package<std::string, int>>>(&results);
+    BOOST_CHECK(packages == nullptr);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
