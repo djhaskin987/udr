2f8b8a60bb352dd23097c3636e600a14eb285aca
Daniel Haskin <djhaskin987@gmail.com>
2016-06-02 13:52:17 -0600

    Add parts necessary for version relation to package

--- src/package.hpp
+++ src/package.hpp
@@ -5,21 +5,35 @@
 #include <boost/variant/variant.hpp>
 #include <string>
 #include <vector>
+#include <unordered_map>
+
 
 namespace udr
 {
-    template <typename N, typename V>
+    
+    template <typename N, typename V> class spec;
+
+    template <
+        typename N,
+        typename V,
+        typename L = std::string>
     struct package
     {
         N name;
         V version;
-        std::string location;
+        // Provides can be used by a repo implementation to present the location
+        // assigned to this package and present it with differerent name/version
+        // pairs, provided in this `provides` map.
+        std::unordered_map<N,V> provides;
+        std::unique_ptr<spec<N,V>> requirements;
+        L location;
     };
     typedef std::vector<std::string> emessages_type;
-    template <typename N, typename V>
+    template <typename N, typename V, typename L = std::string>
     using result_type = boost::variant<
         emessages_type,
-        std::vector<package<N, V> > >;
+        std::vector<package<N,V,L>>
+        >;
 }
 
 #endif // UDR_PACKAGE_HPP
