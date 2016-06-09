cb3baaf98e60af6e25e8131bd54652a57d9bdeac
Daniel Haskin <djhaskin987@gmail.com>
2016-06-09 00:09:54 -0600

    Feature complete for version_relation_spec

--- src/version_relation_spec.hpp
+++ src/version_relation_spec.hpp
@@ -14,6 +14,44 @@ namespace udr
              template <typename T> typename R = std::greater_equal>
     class version_relation_spec : public spec<P>
     {
+    private:
+        class vr_repo : public repository<P>
+        {
+        private:
+            const repository<P>* repo;
+            const typename P::version_type& version;
+            const R<typename P::version_type>& relation;
+        public:
+            vr_repo(const repository<P>* repo,
+                    const typename P::version_type& version,
+                    const R<typename P::version_type>& relation) :
+                repo(repo), version(version), relation(relation) {}
+
+            virtual result_type<P> query(
+                    const typename P::name_type& name) const
+            {
+                auto results = repo->query(name);
+                if (!results)
+                {
+                    return results;
+                }
+                else
+                {
+                    std::vector<P> filtered;
+                    std::copy_if(results->begin(), results->end(),
+                            std::back_inserter(filtered),
+                            [&](const P& thing) {
+                                return relation(version, thing.version);
+                            });
+                    return filtered;
+                }
+            }
+
+            virtual ~vr_repo() {}
+        };
+        name_spec<P> nspec;
+        typename P::version_type version;
+        R<typename P::version_type> relation;
     public:
         version_relation_spec(const typename P::name_type& name,
                 const typename P::version_type& version) :
@@ -22,12 +60,9 @@ namespace udr
         virtual result_type<P>
             resolve(const repository<P>* r) const
         {
-            return udr::none;
+            vr_repo repo(r, version, relation);
+            return nspec.resolve(&repo);
         }
-    private:
-        name_spec<P> nspec;
-        typename P::version_type version;
-        R<typename P::version_type> relation;
     };
 
     template <typename P,
